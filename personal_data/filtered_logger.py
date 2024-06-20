8cc0eb557b0b8ce34d303fb075933d391b6a3209
AntoineJacob <5646@holbertonstudents.com>
2024-06-20 11:54:05 +0200

    task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
-""" task 0. Regex-ing """
+""" task 1. Personnal data """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ accept a list of strings fields constructor argument """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-	""" returns the log message obfuscated """
-	for field in fields:
-		message = re.sub(f'{field}=(.*?){separator}',
+    """ task 0. Regex-ing """
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',
                          f'{field}={redaction}{separator}', message)
-	return message
