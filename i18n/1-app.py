f59cd0e4a8c6bcc4f6c073bfa6392528e72443d6
AntoineJacob <5646@holbertonstudents.com>
2024-07-05 12:21:13 +0200

    fix files 1

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 
 """ Module for trying out Babel i18n """
-
-from flask import Flask, render_template, Babel
+from flask_babel import Babel
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
-
-
-@app.route('/', methods=['GET'], strict_slashes=False)
-def hello_world() -> str:
-    """Renders a Basic Template for Babel Implementation"""
-    return render_template("0-index.html")
 
 
 class Config(object):
@@ -23,6 +17,12 @@ class Config(object):
 
 
 app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world() -> str:
+    """Renders a Basic Template for Babel Implementation"""
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
