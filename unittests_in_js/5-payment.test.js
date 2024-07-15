014f4407927ddfa6f671678dd6338f76dc30f29a
AntoineJacob <5646@holbertonstudents.com>
2024-07-15 14:51:15 +0200

    task 5

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+// 5-payment.test.js
+
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', function() {
+    let log;
+
+    beforeEach(function() {
+        log = sinon.spy(console, 'log');
+    });
+    afterEach(function() {
+        log.restore();
+    });
+
+    it('Checks 100 + 20', function() {
+        sendPaymentRequestToApi(100, 20);
+
+        sinon.assert.calledOnce(log);
+        sinon.assert.calledWith(log, 'The total is: 120');
+    });
+    it('Checks 10 + 10', function() {
+        sendPaymentRequestToApi(10, 10);
+
+        sinon.assert.calledOnce(log);
+        sinon.assert.calledWith(log, 'The total is: 20');
+    });
+});
